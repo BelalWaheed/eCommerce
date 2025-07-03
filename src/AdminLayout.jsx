d62075b9d7497739ca589177c463e199bee36247
belal waheed <belalwaheed11@gmail.com>
2025-07-03 09:15:20 +0300

    Hola

--- src/AdminLayout.jsx
+++ src/AdminLayout.jsx
@@ -5,31 +5,11 @@ import ErrorSection7 from "./pages/user/ErrorSection7";
 import ProductDashboard from "./pages/admin/ProductDashboard";
 import ProductView from "./pages/admin/ProductView";
 import AddNewProduct from "./pages/admin/AddNewProduct";
-import { useSelector } from "react-redux";
 import ViewUsers from "./pages/admin/ViewUsers";
 import EditProduct from "./pages/admin/EditProduct";
 import { AddUser } from "./pages/admin/AddUser";
 import EditUser from "./pages/admin/EditUser";
 function AdminLayout() {
-  const { loading } = useSelector((state) => state.products);
-
-  if (loading) {
-    return (
-      <div className=" bg-gradient-to-l from-gray-50 to-gray-100 dark:from-[#0f172a] dark:to-[#1e293b] transition-transform duration-300">
-        <SimpleNav />
-
-        <div className="flex items-center justify-center h-[calc(100vh-76px)] bg-gray-900 text-white">
-          <div className="flex flex-col items-center space-y-6">
-            <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
-            <p className="text-xl font-semibold animate-pulse">
-              Loading, please wait...
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="bg-gradient-to-l  from-[#0f172a] to-[#1e293b] transition-transform duration-300">
       <SimpleNav />
--- src/UserLayout.jsx
+++ src/UserLayout.jsx
@@ -9,28 +9,8 @@ import { ComplexNavbar } from "./components/user/ComplexNavbar";
 import { Foterr } from "./components/user/Foterr";
 import { Route, Routes } from "react-router-dom";
 import Profile from "./pages/user/Profile";
-import { useSelector } from "react-redux";
 
 function UserLayout() {
-  const { loading } = useSelector((state) => state.products);
-
-  if (loading) {
-    return (
-    <div className="bg-gradient-to-l from-gray-50 to-gray-100 dark:from-[#0f172a] dark:to-[#1e293b] transition-transform duration-300">
-        <ComplexNavbar />
-
-        <div className="flex items-center justify-center h-[calc(100vh-69px)] bg-gray-900 text-white">
-          <div className="flex flex-col items-center space-y-6">
-            <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
-            <p className="text-xl font-semibold animate-pulse">
-              Loading, please wait...
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="bg-gradient-to-l from-gray-50 to-gray-100 dark:from-[#0f172a] dark:to-[#1e293b] transition-transform duration-300">
       <ComplexNavbar />
